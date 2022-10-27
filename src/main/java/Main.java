1341bd4ce3cd5e28e475f2846c361765ae44a3ae
Vsevolod21 <kononenko.vsevolod@gmail.com>
2022-10-27 15:46:11 +0300

    Correction commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,30 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         final var server = new Server();
 
         server.addHandler("GET", "/messages", (request, out) -> {
-                out.write((
-                        "HTTP/1.1 201 Created\r\n" +
-                                "Content-Type: text/plain" +"\r\n" +
-                                "Content-Length: " + 15 + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n" +
-                                "Hello from GET!"
-                        ).getBytes());
-                out.flush();
+            out.write((
+                    "HTTP/1.1 201 Created\r\n" +
+                            "Content-Type: text/plain" + "\r\n" +
+                            "Content-Length: " + 17 + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n" +
+                            "Hello from my GET!"
+            ).getBytes());
+            out.flush();
         });
 
         server.addHandler("POST", "/messages", (request, out) -> {
-                out.write((
-                        "HTTP/1.1 202 Accepted\r\n" +
-                                "Content-Type: " + 0 + "\r\n" +
-                                "Content-Length: " + 0 + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.flush();
+            out.write((
+                    "HTTP/1.1 202 Accepted\r\n" +
+                            "Content-Type: " + 0 + "\r\n" +
+                            "Content-Length: " + 0 + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.flush();
         });
 
         server.listen(9999);
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -28,7 +28,7 @@ public class Server {
     }
 
     public void listen(int port) {
-            if (port <= 0) throw new IllegalArgumentException("Порт должен быть больше нуля");
+        if (port <= 0) throw new IllegalArgumentException("Порт должен быть больше нуля");
         final ExecutorService threadPool = Executors.newFixedThreadPool(64);
         try (final var serverSocket = new ServerSocket(port)) {
             while (true) {
@@ -41,98 +41,98 @@ public class Server {
     }
 
     private void handleConnection(Socket socket) {
-            try(var in = new BufferedInputStream(socket.getInputStream());
-            var out = new BufferedOutputStream(socket.getOutputStream())) {
+        try (var in = new BufferedInputStream(socket.getInputStream());
+             var out = new BufferedOutputStream(socket.getOutputStream())) {
 
-                System.out.println(Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().getName());
 
-                final var limit = 4096;
+            final var limit = 4096;
 
-                in.mark(limit);
-                final var buffer = new byte[limit];
-                final var read = in.read(buffer);
-                // ищем request line
-                final var requestLineDelimiter = new byte[]{'\r', '\n'};
-                final var requestLineEnd =
-                        indexOf(buffer, requestLineDelimiter, 0, read);
-                if (requestLineEnd == -1) {
-                    badRequest(out);
-                    return;
-                }
-                // читаем request line
-                final var requestLine =
-                        new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
-                if (requestLine.length != 3) {
-                    badRequest(out);
-                    return;
-                }
-                System.out.println("Получили реквест лайн: " + Arrays.toString(requestLine));
-
-                // вытаскиваем метод из request line
-                final var method = requestLine[0];
-                if (!allowedMethods.contains(method)) {
-                    badRequest(out);
-                    return;
-                }
-                // вытаскиваем путь из request line
-                final var path = requestLine[1];
-                if (!path.startsWith("/")) {
-                    badRequest(out);
-                    return;
-                }
-                // заголовки
-                final var headersDelimiter = new byte[]{'\r','\n','\r','\n'};
-                final var headersStart = requestLineEnd + headersDelimiter.length;
-                final var headersEnd =
-                        indexOf(buffer, headersDelimiter, headersStart, read);
-                if (headersEnd == -1) {
-                    badRequest(out);
-                    return;
-                }
-                // отматываем на начало буфера
-                in.reset();
-                // пропускаем request line
-                in.skip(headersStart);
-                final var headersBytes = in.readNBytes(headersEnd - headersStart);
-                final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
-
-                final var request = new Request(method, path, headers, null);
-                System.out.println("Итого получился объект реквест: " + request);
-
-                // тело запроса, для GET тела нет
-                if (!method.equals("GET")) {
-                    in.skip(headersDelimiter.length);
-                    final var contentLength = extractHeader(headers, "Content-Length");
-                    if (contentLength.isPresent()) {
-                        final var length = Integer.parseInt(contentLength.get());
-                        final var bodyBytes = in.readNBytes(length);
-                        final var body = new String(bodyBytes);
-                        System.out.println(body);
-                    }
-                }
-
-                var methodMap =
-                        allHandlers.get(request.getMethod());
-                System.out.println("мапа методмап: " + methodMap);
-
-                if (methodMap == null) {
-                    notFound(out);
-                    return;
-                }
-
-                var handler = methodMap.get(request.getPath());
-                System.out.println("хендлер: " + handler);
-
-                if (handler == null) {
-                    notFound(out);
-                    return;
-                }
-
-                handler.handle(request, out);
-
-            } catch (IOException e) {
-                e.printStackTrace();
+            in.mark(limit);
+            final var buffer = new byte[limit];
+            final var read = in.read(buffer);
+            // ищем request line
+            final var requestLineDelimiter = new byte[]{'\r', '\n'};
+            final var requestLineEnd =
+                    indexOf(buffer, requestLineDelimiter, 0, read);
+            if (requestLineEnd == -1) {
+                badRequest(out);
+                return;
             }
+            // читаем request line
+            final var requestLine =
+                    new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+            if (requestLine.length != 3) {
+                badRequest(out);
+                return;
+            }
+            System.out.println("Получили реквест лайн: " + Arrays.toString(requestLine));
+
+            // вытаскиваем метод из request line
+            final var method = requestLine[0];
+            if (!allowedMethods.contains(method)) {
+                badRequest(out);
+                return;
+            }
+            // вытаскиваем путь из request line
+            final var path = requestLine[1];
+            if (!path.startsWith("/")) {
+                badRequest(out);
+                return;
+            }
+            // заголовки
+            final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+            final var headersStart = requestLineEnd + headersDelimiter.length;
+            final var headersEnd =
+                    indexOf(buffer, headersDelimiter, headersStart, read);
+            if (headersEnd == -1) {
+                badRequest(out);
+                return;
+            }
+            // отматываем на начало буфера
+            in.reset();
+            // пропускаем request line
+            in.skip(headersStart);
+            final var headersBytes = in.readNBytes(headersEnd - headersStart);
+            final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+
+            final var request = new Request(method, path, headers, null);
+            System.out.println("Итого получился объект реквест: " + request);
+
+            // тело запроса, для GET тела нет
+            if (!method.equals("GET")) {
+                in.skip(headersDelimiter.length);
+                final var contentLength = extractHeader(headers, "Content-Length");
+                if (contentLength.isPresent()) {
+                    final var length = Integer.parseInt(contentLength.get());
+                    final var bodyBytes = in.readNBytes(length);
+                    final var body = new String(bodyBytes);
+                    System.out.println(body);
+                }
+            }
+
+            var methodMap =
+                    allHandlers.get(request.getMethod());
+            System.out.println("мапа методмап: " + methodMap);
+
+            if (methodMap == null) {
+                notFound(out);
+                return;
+            }
+
+            var handler = methodMap.get(request.getPath());
+            System.out.println("хендлер: " + handler);
+
+            if (handler == null) {
+                notFound(out);
+                return;
+            }
+
+            handler.handle(request, out);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void badRequest(BufferedOutputStream out) throws IOException {
@@ -141,9 +141,10 @@ public class Server {
                         "Content-Length: 0\r\n" +
                         "Connection: close\r\n" +
                         "\r\n"
-                ).getBytes());
+        ).getBytes());
         out.flush();
     }
+
     private void notFound(BufferedOutputStream out) throws IOException {
         out.write((
                 "HTTP/1.1 404 Not Found!\r\n" +
@@ -153,6 +154,7 @@ public class Server {
         ).getBytes());
         out.flush();
     }
+
     private int indexOf(byte[] array, byte[] target, int start, int max) {
         outer:
         for (int i = start; i < max - target.length + 1; i++) {
@@ -165,6 +167,7 @@ public class Server {
         }
         return -1;
     }
+
     private Optional<String> extractHeader(List<String> headers, String header) {
         return headers.stream()
                 .filter(o -> o.startsWith(header))
